f585495d39e8be060cfb9ef0c16d5e35bbf492ed
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-16 07:09:57 +0530

    feat: wrap SignIn and SignUp components in a centered layout and add UserButton to Home component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div className="text-3xl">Home </div>;
+  return <div className="text-3xl">Home
+  
+  <UserButton/> </div>;
 };
 
 export default Home;
