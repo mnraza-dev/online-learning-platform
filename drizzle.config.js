d9fe4a933017b2299a1e289cba446f4aa63e9640
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-16 07:35:05 +0530

    feat: update schema file extension in drizzle configuration

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,9 @@
-import 'dotenv/config';
+
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './config/schema.ts',
+  schema: './config/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
