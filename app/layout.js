4d5f31698d1fa6b8ae8dfdab099cd91e1dfd327f
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-16 06:39:21 +0530

    refactor: replace Geist font with Inter font in layout

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -19,10 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className} `}>
+        <main className="min-h-screen">{children}</main>
       </body>
     </html>
   );
