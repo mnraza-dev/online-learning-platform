e22fd0e38df1fdf9f39e80e846978cab90cdd4b7
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-16 08:08:32 +0530

    feat: make subscriptionId optional in usersTable schema

--- config/schema.js
+++ config/schema.js
@@ -4,5 +4,5 @@ export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  subscriptionId: varchar({ length: 255 }).notNull(),
+  subscriptionId: varchar({ length: 255 }),
 });
