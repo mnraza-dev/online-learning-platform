c77b1329ff8f051f35378de2e971f3430153eda2
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-16 07:55:24 +0530

    feat: refactor Provider component to create new user on user change and add axios dependency

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,13 +1,24 @@
-"use client"
-import React from 'react'
+"use client";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import React, { useEffect } from "react";
 
-const Provider = ({children}) => {
-  const CreateNewUser = () => {
+const Provider = ({ children }) => {
+  const { user } = useUser();
 
-  }
-  return (
-    <div>{children}</div>
-  )
-}
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user]);
 
-export default Provider
+  const CreateNewUser = async () => {
+    const result = await axios.post("/api/user", {
+      name: user?.firstName + " " + user?.lastName,
+      email: user?.primaryEmailAddress?.emailAddress,
+    });
+
+    console.log(result.data);
+  };
+  return <div>{children}</div>;
+};
+
+export default Provider;
