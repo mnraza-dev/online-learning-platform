4017cd193592fcf26da540efc27990512040c9be
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-17 23:51:26 +0530

    feat: update AppSidebar to adjust logo size and add "Create New Course" button

--- app/workspace/_components/AppSidebar.jsx
+++ app/workspace/_components/AppSidebar.jsx
@@ -7,15 +7,20 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
 import Image from "next/image"
+import { Button } from "@/components/ui/button"
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader >
-        <Image src={"/logo.svg"} alt="Logo" width={100} height={100}/>
+        <Image src={"/logo.svg"} alt="Logo" width={140} height={120}/>
         </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup >
+          <Button>
+            Create New Course
+          </Button>
+          </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
