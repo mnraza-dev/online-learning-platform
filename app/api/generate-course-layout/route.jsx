c2399ecc75fc11536bae832c0c6285a1c329df5f
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-18 20:27:01 +0530

    feat: refactor POST function to integrate course generation prompt and handle user input in JSON format

--- app/api/generate-course-layout/route.jsx
+++ app/api/generate-course-layout/route.jsx
@@ -1,25 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-
-export async function POST(req) {
-  const formData = await req.json();
-  // To run this code you need to install the following dependencies:
-  // npm install @google/genai mime
-  // npm install -D @types/node
-
-  async function main() {
-    const ai = new GoogleGenAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    });
-    const config = {
-      responseMimeType: "text/plain",
-    };
-    const model = "gemini-2.5-pro-preview-05-06";
-    const contents = [
-      {
-        role: "user",
-        parts: [
-          {
-            text: `
+const PROMPT = `
 Generate Learning Course depends on following details. In which Make sure to add course name, description, Chapter Name, image prompt (Create a modern, flat-style 2D digital illustration representing user Topic. Include UI/UX elements such as mockup screens, text blocks, icons, buttons , and creative workspace tools. Add symbolic elements related to user Course, like sticky notes, design components, and visual aids. Use a vibrant color palette ( blues, purple, oranges) with a clean, professional look. The illustration should feel creative , tech savvy and and educational, ideal for visualizing concepts in user Course) for Course Banner in 3D format, Topic under each chapters, Duration for each chapters etc, in JSON format only
 Schema:
 {
@@ -40,11 +20,22 @@ Schema:
 }
 },
 User Input : Reactjs , 3 chapters
-
-
-
-
-`,
+`
+export async function POST(req) {
+  const formData = await req.json();
+    const ai = new GoogleGenAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    });
+    const config = {
+      responseMimeType: "text/plain",
+    };
+    const model = "gemini-2.5-pro-preview-05-06";
+    const contents = [
+      {
+        role: "user",
+        parts: [
+          {
+            text: PROMPT+JSON.stringify(formData),
           },
         ],
       },
@@ -437,6 +428,3 @@ These prompts are more specific and directly use the template. Looks good.`,
       console.log(chunk.text);
     }
   }
-
-  main();
-}
