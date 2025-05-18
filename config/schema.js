e8f6a5bbd4d672cf14a56298c86b13385d055a1b
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-18 09:45:33 +0530

    feat: add boolean import to schema.js for course attributes

--- config/schema.js
+++ config/schema.js
@@ -1,4 +1,4 @@
-import { integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, json, pgTable, varchar , boolean} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
